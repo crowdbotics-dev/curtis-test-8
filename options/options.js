400fd94fc636f3459f2f0b2e7f8fa05b4fa9c610
curtis@crowdbotics.com <curtis@crowdbotics.com>
2023-05-11 14:55:27 -0600

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "curtis-test-8",
-  url: "https://curtis-test-8.botics.co",
-  api: "https://curtis-test-8.botics.co/api/v1"
-}
+  "name": "curtis-test-8",
+  "url": "https://curtis-test-8.botics.co",
+  "api": "https://curtis-test-8.botics.co/api/v1",
+  "initialRoute": "Untitled1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
