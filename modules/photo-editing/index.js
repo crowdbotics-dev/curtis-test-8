424d6b403d66538fe0c7be041dc95fcb9f64c0ca
Crowdbotics <engineering@crowdbotics.com>
2023-05-15 22:20:00 +0000

    add component Photo editing

--- modules/photo-editing/index.js
+++ modules/photo-editing/index.js
@@ -4,14 +4,17 @@ import React from "react";
 import { Provider } from "react-redux";
 import App from "./App";
 import rootReducer from "./Store";
+
 const store = configureStore({
   reducer: rootReducer
 });
 
 const PhotoEditing = () => {
-  return <Provider store={store}>
+  return (
+    <Provider store={store}>
       <App />
-    </Provider>;
+    </Provider>
+  );
 };
 
 export default {
